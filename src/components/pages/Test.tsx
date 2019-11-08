00623f1781ce5c2400ecf1e8f1d52fdbf0d4898f
Joao Morgado <joaomorgado0@gmail.com>
2019-11-08 14:40:41 +0000

    *Change: removed unused code on test

--- src/components/pages/Test.tsx
+++ src/components/pages/Test.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { AppContext  } from '../../common/config/appConfig';
 import { useServiceCaller } from '../../common/services/serviceCaller';
 import { ServiceType } from '../../common/services/serviceCallerInterfaces';
@@ -20,39 +20,6 @@ interface IResult {
   text: string;
 }
 
-const getResults = ( search?: string ): IResult[] => {
-  let resultList: IResult[] = [
-      {
-        id: 0,
-        text: "Result zero"
-      },
-      {
-        id: 1,
-        text: "Result one"
-      },
-      {
-        id: 2,
-        text: "Result two"
-      },
-      {
-        id: 3,
-        text: "Result three"
-      },
-      {
-        id: 4,
-        text: "Result four"
-      }
-    ];
-
-  if( search === undefined || search === "" )
-  {
-    return resultList;
-  }
-
-  return resultList.filter( r => r.id.toString().includes( search ) || r.text.includes( search ) );
-}
-
-
 const serverCallTest: ServiceType<IResult, IResult> = ( context, request ) => {
   if( request === undefined ) 
     return {
@@ -73,33 +40,12 @@ const serverCallTest: ServiceType<IResult, IResult> = ( context, request ) => {
   };
 }
 
-function loot( text:string, oldLenght: number, lootBox: boolean ){
-  if( lootBox && text.length > oldLenght && text !== "" )
-  {
-    let newText = text.substring( 0, text.length - 1 );
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&/()=?@£§{[]}«»´`~^ºª*+¨-_.:,;| ';
-    newText += characters.charAt(Math.floor(Math.random() * characters.length));
-    return newText;
-  }
-
-  return text;
-}
-
-const Test: React.FC = () => {
-  const [search, setSearch] = useState<string>( "" );
-  const [results, setResults] = useState<IResult[]>( [] );
-  const [showTable, setShowTable] = useState<boolean>( true );
-  const [lootBox, setLootBox] = useState<boolean>( true );
+const Test: React.FC = () => {  
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [appContext] = useContext( AppContext );
   const [serviceResponse, serviceHandler] = useServiceCaller<IResult, IResult>( serverCallTest );
   const [serviceResponse2, serviceHandler2] = useServiceCaller<IResult, IResult>( serverCallTest, ErrorCodes.GenericError, true );
   const serviceHandler3 = useServiceCaller<IResult, IResult>( serverCallTest )[1];
-
-  useEffect(() => {
-    setResults( getResults( search ) );
-    document.title = "Kjohn React Framework: " + search;
-  }, [search]);
 
   const loadService2 = () => {
     setIsLoading( true );
@@ -118,33 +64,7 @@ const Test: React.FC = () => {
   return (
     <div className="TestApp">
       { "Hash token tested correctly: " + testhash( "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c", "7f75367e7881255134e1375e723d1dea8ad5f6a4fdb79d938df1f1754a830606" ) }
-      <div className = "searchBox">
-        <label>{appContext.translations.testPage.searchBox}</label>
-        <input 
-          type = "text" 
-          onChange = { (event: React.FormEvent<HTMLInputElement>) => setSearch( loot( event.currentTarget.value, search.length, lootBox ) ) }
-          value = { search }
-        />
-        <label>&nbsp;Loot?&nbsp;</label>
-        <input type="checkbox" checked = {lootBox} onChange = { (event: React.FormEvent<HTMLInputElement>) => { setLootBox( event.currentTarget.checked ) } }/>
-        <hr />      
-      </div>
-      <div className = "testtable">
-        <div className = "resultTable tabletestHeader">
-          <span className="leftCol testheader">ID</span><span className="rightCol testheader">{appContext.translations.testPage.text}</span>
-        </div>
-        { showTable && results.map( (r, i) => 
-          <div className = "resultTable" key = { i }>
-            <span className="leftCol">{r.id}</span><span className="rightCol" >{r.text}</span>
-          </div>
-        )}
-        <WithTooltip className = "tableHideToolTip" toolTipText = "Show or Hide the table" toolTipColor = { ToolTipColor.Blue } toolTipPosition = { ToolTipPosition.Bottom }>
-          <div className = "tableHide" onClick = { () => { setShowTable( !showTable ) } } >
-            <span> { showTable ? <span>&uarr;</span> : <span>&darr;</span> } </span>
-          </div>
-        </WithTooltip>
-      </div>
-      <div className = "testtableBottom">
+      <div className = "TestPageStuff">
         <hr />
         <div className = "center_menu_button">
           <WithTooltip toolTipText = { appContext.translations.testPage.serviceCallTooltip1 } toolTipPosition = { ToolTipPosition.Top } toolTipColor = { ToolTipColor.Green }  >
@@ -212,7 +132,7 @@ const Test: React.FC = () => {
               [
                 { text: "01" },
                 { text: "02 AASASDSFSDFS" },
-                { text: "03 with a lot of text lalala lorem ipsum and dont say anything", onClickEdit: () => {}, onClickRemove: () => {}, toolTip: "This is a cell with edit", toolTipColor: ToolTipColor.Grey }
+                { text: "03 with a lot of text lalala lorem ipsum and dont say anything", onClickEdit: () => {}, onClickRemove: () => {}, toolTip: "This is a cell with edit", toolTipColor: ToolTipColor.Blue }
               ],
               [
                 { text: "04 AASASDSFSDFS" },
