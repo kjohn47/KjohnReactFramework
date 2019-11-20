d033d799db0c068a138d11d26f8f280dad36d908
Joao Morgado <joaomorgado0@gmail.com>
2019-11-20 10:47:14 +0000

    *Change:[KRF-00001] improved fetch, error and translation handling

--- src/common/context/pageText/getTranslation.tsx
+++ src/common/context/pageText/getTranslation.tsx
@@ -1,17 +1,33 @@
 import { useContext } from "react"
 import { AppContext } from "../../config/appConfig"
+import { ITranslations } from "../appContextInterfaces";
+import { AppLanguage } from "../appContextEnums";
 
-const useTranslation: () => { getTranslation: ( process: string, token: string ) => string } = () => {
-    const [appContext] = useContext( AppContext );
-    const getTranslation: ( process: string, token: string ) => string = ( process, token ) => {
-        let translation = appContext.translations[ appContext.globalLanguage ];
-        let translatedProcess = translation !== undefined ? translation[ process ] : undefined;
-        if( translatedProcess === undefined )
-            return "#([" + process + "]." + token + ")";
-        let translatedText = translatedProcess[ token ];
-        if( translatedText === null || translatedText === undefined || translatedText === "" )
-            return "#(" + process + ".[" + token + "])";
-        return translatedText as string;
+const getToken: ( process: string, token: string, translations: ITranslations, language: AppLanguage ) => string = ( process, token, translations, language ) => {
+    let translation = translations[ language ];
+    if ( translation === undefined )
+        return `[${ process }.${ token }]`;
+    let translatedProcess = translation[ process ];
+    if ( translatedProcess === undefined )
+        return `[${ process }].${ token }`;
+    let translatedText = translatedProcess[ token ];
+    if ( translatedText === null || translatedText === undefined || translatedText === "" )
+        return `${ process }.[${ token }]`;
+
+    return translatedText;
+}
+
+const useTranslation: () => { getTranslation: ( process: string, token: string, args?: string[] ) => string } = () => {
+    const [ appContext ] = useContext( AppContext );
+    const getTranslation: ( process: string, token: string, args?: string[] ) => string = ( process, token, args?) => {
+        let textOut = getToken( process, token, appContext.translations, appContext.globalLanguage );
+        if ( args && args.length > 0 ) {
+            args.forEach( ( arg, i ) => {
+                textOut = textOut.replace( `{${ i }}`, arg );
+            } )
+        }
+
+        return textOut;
     }
     return { getTranslation };
 }
