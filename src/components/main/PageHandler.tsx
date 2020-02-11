d51fe547f79fd478b8bbf62ed38072759982d6fe
Joao Morgado <joaomorgado0@gmail.com>
2020-02-11 17:44:27 +0000

    !Fix: [KRF-00001] fixed page handler and error handler refresh

--- src/components/main/PageHandler.tsx
+++ src/components/main/PageHandler.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AppContext, ErrorContext, LoadingContext } from "../../common/config/appConfig";
 import { KnownPages } from "../../common/context/appContextEnums";
 import { ErrorCodes, ErrorActions } from "../../common/context/appErrorEnums";
@@ -22,44 +22,37 @@ export interface IPageHandleProps {
     };
 }
 
-const PageHandler: React.FC<IPageHandleProps> = ({Routes}) => {
+const PageHandler: React.FC<IPageHandleProps> = ( { Routes } ) => {
     const [ appContext ] = useContext( AppContext );
     const [ errorContext, setErrorContext ] = useContext( ErrorContext );
     const isLoading = useContext( LoadingContext )[ 0 ];
-    let selectedPage = errorContext.hasError ? KnownPages.Error : appContext.selectedPage;
+    const [ output, setOutput ] = useState<React.ComponentType>();
 
-    let Output: React.ComponentType | undefined;
-
-    if( selectedPage.toLowerCase() === KnownPages.Home.toLowerCase() )
-    {
-        Output = injectProps( Routes.Home.Component, Routes.Home.Props );        
-    }
-    else if( selectedPage.toLowerCase() === KnownPages.Error.toLowerCase())
-    {
-        Output = ErrorPage;
-    }
-    else
-    {
-        let route = Routes.KnownRoutes && Routes.KnownRoutes.filter( r => r.Route.toLowerCase() === selectedPage.toLowerCase() )[0];        
-        if( route )
-        {
-            Output = injectProps( route.Component, route.Props );
+    useEffect( () => {
+        if ( !errorContext.hasError ) {
+            let selectedPage = appContext.selectedPage;
+            if ( selectedPage.toLowerCase() === KnownPages.Home.toLowerCase() ) {
+                setOutput( injectProps( Routes.Home.Component, Routes.Home.Props ) );
+            }
+            else {
+                let route = Routes.KnownRoutes && Routes.KnownRoutes.filter( r => r.Route.toLowerCase() === selectedPage.toLowerCase() )[ 0 ];
+                if ( route ) {
+                    setOutput( injectProps( route.Component, route.Props ) );
+                }
+                else {
+                    setErrorContext( {
+                        type: ErrorActions.ActivateError,
+                        errorCode: ErrorCodes.PageNotFound,
+                        errorDescription: "Not Found: 404"
+                    } );
+                }
+            }
         }
-    }
-    
-    //getKnownPage( selectedPage );
-
-    if ( Output === undefined ) {
-        setErrorContext( {
-            type: ErrorActions.ActivateError,
-            errorCode: ErrorCodes.PageNotFound,
-            errorDescription: "Not Found: 404"
-        } );
-    }
+    }, [ appContext.selectedPage, appContext.queryString, Routes, setErrorContext, errorContext.hasError ] )
 
     return (
         <Loader isLoading={ isLoading } bigLoader paddingTop >
-            { Output && <Output /> }
+            { !errorContext.hasError && output ? output : <ErrorPage /> }
         </Loader>
     );
 }
