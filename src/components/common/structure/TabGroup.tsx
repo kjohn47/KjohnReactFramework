7a363d677ba203ebc5434fbda10cc0b3ad4a6efc
Joao Morgado <joaomorgado0@gmail.com>
2020-09-29 16:45:24 +0100

    *change: [KRF-00001] changed Tab group component to collapse on mobile or low width viewport

--- src/components/common/structure/TabGroup.tsx
+++ src/components/common/structure/TabGroup.tsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import Column from './Column';
+import React, { useState, useEffect, useMemo, useCallback, useRef } from 'react';
+import Column, { ColumnNumber } from './Column';
 import Row from './Row';
 import { injectProps } from '../../../logic/functions/misc';
+import { useMobileWidth } from '../../../logic/functions/windowResize';
+import { mobileWidthMenu } from '../../../logic/config/configuration';
 
 export interface ITabGroup {
     Tabs: ITabItem<any>[]
@@ -17,11 +19,72 @@ export interface ITabItem<TProps> {
 const TabGroup: React.FC<ITabGroup> = (props) => {
     const [selectedIndex, setSelectedIndex] = useState<number>( props.DefaultIndex ? props.DefaultIndex : 0);
     const [selectedComponent, setSelectedComponent] = useState<React.ComponentType | undefined>(undefined);
-    const indexHandle: (index: number) => void = (index) =>
+    const [collapsedTabs, setCollapsedTabs] = useState<boolean>(false);
+    const collapsedMenuRef = useRef<HTMLDivElement>(null);
+    
+    const maxWith = useMemo(() => {
+        const numMenus: number = props.Tabs !== undefined ? props.Tabs.length : 0;
+        return numMenus <= 2 ? mobileWidthMenu : mobileWidthMenu + ( 150 * (numMenus - 2) );
+      }, [props.Tabs])
+
+    const isCollapsed = useMobileWidth(maxWith).isCustomWidth;
+
+    const indexHandle = useCallback((index: number, closeCollapse?: boolean): void =>
     {
         if(index !== selectedIndex)
         {
             setSelectedIndex(index);
+        }
+
+        if(closeCollapse)
+        {
+            setCollapsedTabs(false);
+        }
+
+    },[selectedIndex]);
+
+    const generateTabs = useMemo(() => {
+        return (
+        <Row className="TabGroupTabRow">
+        {
+            props.Tabs.map( (tab, i) =>
+                <Column 
+                    key = {"Tab_" + i}
+                    className = {"TabItem noselect" + (selectedIndex !== i ? " pointer_cursor" : " SelectedTab")}
+                    onClick = {() => indexHandle(i)}
+                >
+                    {
+                        tab.Title
+                    }
+                </Column>
+            )
+        }
+        </Row>
+    )}, [props.Tabs, selectedIndex, indexHandle])
+
+    const generateCollapsedTabs = useMemo(() => {
+        return (
+        <div className="CollapsedTabMenu">
+        {
+            props.Tabs.map( (tab, i) =>
+                <div 
+                    key = {"CollapsedTab_" + i}
+                    className = {"CollapsedTabItem noselect" + (selectedIndex !== i ? " pointer_cursor" : " CollapsedTabSelected")}
+                    onClick = {() => indexHandle(i, true)}
+                >
+                    {
+                        tab.Title
+                    }
+                </div>
+            )
+        }
+        </div>
+    )}, [props.Tabs, selectedIndex, indexHandle])
+
+    const handleClickOut = (event: any) => {
+        if(isCollapsed && collapsedTabs && collapsedMenuRef.current && !collapsedMenuRef.current.contains(event.target))
+        {
+            setCollapsedTabs(false);
         }
     }
 
@@ -32,24 +95,37 @@ const TabGroup: React.FC<ITabGroup> = (props) => {
         }
     },[props, selectedIndex])
 
+    useEffect(() => {
+        // add when mounted
+        document.addEventListener( "mousedown", handleClickOut );
+        // return function to be called when unmounted
+        return () => {
+        document.removeEventListener( "mousedown", handleClickOut );
+        };
+        // eslint-disable-next-line
+    }, [collapsedTabs]);
+
+    useEffect(() => {
+        if(!isCollapsed)
+        {
+            setCollapsedTabs(false);
+        }
+    }, [isCollapsed])
+
     return (
         <Row className="TabGroup">
             <Column>
-                <Row className="TabGroupTabRow">
-                    {
-                        props.Tabs.map( (tab, i) =>
-                            <Column 
-                                key = {"Tab_" + i}
-                                className = {"TabItem noselect" + (selectedIndex !== i ? " pointer_cursor" : " SelectedTab")}
-                                onClick = {() => indexHandle(i)}
-                            >
-                                {
-                                    tab.Title
-                                }
-                            </Column>
-                        )
-                    }
-                </Row>
+                {isCollapsed ? 
+                    <Row className="TabGroupTabRowCollapsed">
+                        <Column full={ColumnNumber.C2} reference={collapsedMenuRef}>
+                            <span className = {`TabMenuEntry pointer_cursor${(collapsedTabs ? " TabEntrySelected": "")}`} onClick = {() => setCollapsedTabs(prev => !prev)}>|||</span>
+                            {collapsedTabs ? generateCollapsedTabs : null}
+                        </Column>
+                        <Column full={ColumnNumber.C18}>
+                            {props.Tabs[selectedIndex].Title}
+                        </Column>
+                    </Row>
+                    : generateTabs}
                 <Row className="TabGroupContentRow">
                     <Column>
                         {selectedComponent}
